c05b1db4aa227cfbdac800038ef24d91db294a37
Jayt4c <igejuwon53@gmail.com>
2023-12-05 06:13:58 -0800

    shell head

--- shell.h
+++ shell.h
@@ -8,8 +8,12 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <dirent.h>
+#include <fcntl.h>
 
 
+void execute_file(const char *filename);
+void non_interactive_mode(const char *filename);
+void interactive_mode(void);
 void printout(const char *string);
 void display_prompt(void);
 void get_input(char *command, size_t size);
