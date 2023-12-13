824142a80b3e40d56c2a2bfc46103bb59369f723
Jayt4c <igejuwon53@gmail.com>
2023-12-13 05:20:26 -0800

    changed fgets to getline

--- jashell.c
+++ jashell.c
@@ -7,12 +7,15 @@
 
 int main(void)
 {
-	char command[1024];
+	/*char command[1024];*/
 
 	for (;;)
 	{
+		char *command = NULL;
+		size_t size = 0;
+
 		display_prompt();
-		get_input(command, sizeof(command));
+		get_input(&command, &size);
 		if (strlen(command) == 0)
 		{
 			continue;
@@ -23,10 +26,18 @@ int main(void)
 			break;
 		}
 		if ((command[0] >= 'a' && command[0] <= 'z') || (command[0] >= 'A'
-			&& command[0] <= 'Z') || command[0] == '/')
+			&& command[0] <= 'Z'))
+		{
+			/*path_handler(command);*/
+			execute_promptcommand(command);
+		} else if (command[0] == '/')
 		{
 			path_handler(command);
 		}
+
+		free(command);
+		command = NULL;
+		size = 0;
 	}
 
 	return (0);
